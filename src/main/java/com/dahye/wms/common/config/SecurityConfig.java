d4b715e82bbe2c7d63a05274137ed95617c3c12c
ekgpgdi <dlekgp0423@naver.com>
2025-02-12 17:44:18 +0900

    feat: security 및 공통 응답 포맷 설정

---
+++ src/main/java/com/dahye/wms/common/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.dahye.wms.common.config;
+
+import com.dahye.wms.common.filter.JwtAuthenticationFilter;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/h2-console/**", "/favicon.ico");
+    }
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomAccessDeniedHandler customAccessDeniedHandler, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter; // 생성자 주입
+        this.customAccessDeniedHandler = customAccessDeniedHandler;
+        this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorize ->
+                                authorize
+                                        .requestMatchers("/api/v1/auth/**", "/api/v1/product/**", "/api-swagger.html", "/swagger-ui/**", "/api-docs/**", "/h2-console/**")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated())
+                .exceptionHandling(e -> e.authenticationEntryPoint(customAuthenticationEntryPoint)
+                .accessDeniedHandler(customAccessDeniedHandler))
+                .csrf(csrf -> csrf.ignoringRequestMatchers(PathRequest.toH2Console()));
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/dahye/wms/common/domain/ResponseCode.java
@@ -0,0 +1,39 @@
+package com.dahye.wms.common.domain;
+
+public enum ResponseCode {
+    // success
+    SUCCESS(false),
+
+    // auth
+    UNAUTHORIZED(true),
+    FORBIDDEN(true),
+
+    // validation
+    REQUIRED_EMAIL(true),
+    REQUIRED_PASSWORD(true),
+    REQUIRED_NICKNAME(true),
+    EMAIL_FORMAT_ERROR(true),
+    CHECK_CERT_CODE(false),
+    VERIFIED_CODE_ERROR(false),
+    REQUIRED_EMAIL_VERIFIED(true),
+    REQUIRED_PRODUCT_ID(true),
+    INVALID_PASSWORD(false),
+    PRODUCT_OUT_OF_STOCK(false),
+
+    // not found
+    NOT_FOUND_CUSTOMER(false),
+    NOT_FOUND_PRODUCT(true),
+
+    // exist
+    EXIST_EMAIL(false),
+
+    // sever error
+    FAIL_PAY(true),
+    SERVER_ERROR(true);
+
+    public final boolean isFatality;
+
+    ResponseCode(boolean isFatality) {
+        this.isFatality = isFatality;
+    }
+}
